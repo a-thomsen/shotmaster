df0859cd666d9c3230f55795e9d5669efd788850
Anna <73801786+a-thomsen@users.noreply.github.com>
2021-10-24 00:07:25 +0300

    23/10/2021

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Uncover the secrets',
+  headline: 'Frame the unexpected',
+  description: 'Discover the importance of frames: catch the essence of what it takes to get into the right place at the right time.',
+  buttonLabel: 'Join the greats',
+  imgStart: false,
+  img: require('../../images/petrobras.jpg'),
+  alt: 'Petrol station',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Find your voice',
+  headline: 'Unleash the key elements of the process',
+  description: 'Gain treasured insight to master the method. Get confident through making and editing your photography.',
+  buttonLabel: 'Creativity starts here',
+  imgStart: true,
+  img: require('../../images/alley.jpg'),
+  alt: 'Dark alley',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Exploit our brain',
+  headline: 'Pick the masterminds of still pictures',
+  description: 'From characteristic mugshots to the pages of Time – delve into the techniques of the marvels of modern era',
+  buttonLabel: 'Take the plunge',
+  imgStart: false,
+  img: require('../../images/crossing.jpg'),
+  alt: 'Crossing the road',
+  dark: false,
+  primary: false,
+  darkText: true
+};
